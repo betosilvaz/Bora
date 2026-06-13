3aab8b37b35c40db350df598b246bbada1c04e1d
Gilberto Silva <gilbertozn527@gmail.com>
2026-06-13 12:21:21 -0300

    feat: injeta estado na MenuScreen.kt e adiciona callback de logout

--- app/src/main/java/com/example/bora/MainActivity.kt
+++ app/src/main/java/com/example/bora/MainActivity.kt
@@ -6,24 +6,31 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import com.example.bora.localStorage.LocalStorage
 import com.example.bora.ui.components.BottomBar
 import com.example.bora.ui.screen.add.AddScreen
 import com.example.bora.ui.screen.events.EventsScreen
 import com.example.bora.ui.screen.forgotPassword.ForgotPasswordScreen
 import com.example.bora.ui.screen.login.LoginScreen
+import com.example.bora.ui.screen.login.LoginViewModel
 import com.example.bora.ui.screen.map.MapScreen
 import com.example.bora.ui.screen.menu.MenuScreen
+import com.example.bora.ui.screen.menu.MenuViewModel
 import com.example.bora.ui.screen.search.SearchScreen
 import com.example.bora.ui.screen.signup.SignupScreen
+import com.example.bora.ui.screen.signup.SignupViewModel
 import com.example.bora.ui.theme.BoraTheme
 
 class MainActivity : ComponentActivity() {
@@ -86,20 +93,43 @@ fun Router() {
             )
         }
     }) { paddingValues ->
-        val padding = paddingValues
         NavHost(navController = navController, startDestination = startDestination) {
             navigation(route = "auth", startDestination = Route.Login.route) {
                 composable(Route.Login.route) {
+                    val viewModel: LoginViewModel = viewModel()
+                    val state by viewModel.uiState.collectAsStateWithLifecycle()
+
+                    LaunchedEffect(state.isSuccess) {
+                        if (state.isSuccess) {
+                            navController.navigate(Route.Map.route) {
+                                popUpTo(Route.Login.route) { inclusive = true }
+                            }
+                        }
+                    }
+
                     LoginScreen(
-                        onClickLogin = {  },
+                        state = state,
+                        onClickLogin = { viewModel.login() },
                         onClickSignup = { navController.navigate(Route.Signup.route) },
                         onClickForgotPassword = { navController.navigate(Route.ForgotPassword.route) },
                     )
                 }
                 composable(Route.Signup.route) {
+                    val viewModel: SignupViewModel = viewModel()
+                    val state by viewModel.uiState.collectAsStateWithLifecycle()
+
+                    LaunchedEffect(state.isSuccess) {
+                        if (state.isSuccess) {
+                            navController.navigate(Route.Map.route) {
+                                popUpTo(Route.Signup.route) { inclusive = true }
+                            }
+                        }
+                    }
+
                     SignupScreen(
+                        state = state,
                         onClickLogin = { navController.navigate(Route.Login.route) },
-                        onSignup = {  },
+                        onSignup = { viewModel.signup() },
                     )
                 }
                 composable(Route.ForgotPassword.route) {
@@ -123,9 +153,19 @@ fun Router() {
                     EventsScreen()
                 }
                 composable(Route.Menu.route) {
-                    MenuScreen(
-                        onLogout = {  },
+                    val viewModel: MenuViewModel = viewModel()
+                    val state by viewModel.uiState.collectAsStateWithLifecycle()
 
+                    viewModel.updateUsername(LocalStorage.getItem("username") ?: "Usuário Desconhecido")
+
+                    MenuScreen(
+                        state = state,
+                        onLogout = {
+                            viewModel.logout()
+                            navController.navigate(Route.Login.route) {
+                               popUpTo(Route.Menu.route) { inclusive = true }
+                            }
+                        },
                     )
                 }
             }
